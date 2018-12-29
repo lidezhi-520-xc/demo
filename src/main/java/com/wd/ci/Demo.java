e40c1498d8b001b45c5c4392727751a62c572302
lidezhiwangxiaocui <1013983334@qq.com>
2018-12-29 09:44:05 +0800

    创建demo项目

---
+++ src/main/java/com/wd/ci/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.wd.ci;
+
+/**
+ * @author lidezhi
+ *
+ */
+public class Demo {
+
+	private String appName = "Good apple,test jenkins";
+
+	public String getAppName() {
+		return appName;
+	}
+
+	public void setAppName(String appName) {
+		this.appName = appName;
+	}
+}
